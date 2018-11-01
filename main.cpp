9927e9cdf69629b3e6b04d357dabb29eb3379b67
lStewieAl <lStewieAl@users.noreply.github.com>
2018-11-01 03:21:05 +0000

    Added skeleton calls for ctrl left/right, backspace/delete
    Also created function getConsoleInputString, which return the string currently in the console input.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@
 #include <string>
 #include <windows.h>
 
-IDebugLog  gLog("nvse_plugin_console_clipboard.log");
+#ifdef NOGORE
+IDebugLog		gLog("nvse_plugin_console_clipboard.log");
+#else
+IDebugLog		gLog("nvse_plugin_console_clipboard.log");
+#endif
 
 NVSEInterface * SaveNVSE;
 DIHookControl *g_DIHookCtrl = NULL;
@@ -16,7 +20,11 @@ void patchOnConsoleInput();
 void __fastcall CheckCTRLV();
 void GetClipboardText(char** buffer);
 void __fastcall PrintToConsoleInput(UInt32 character);
+void logConsoleInput(char* bufferLocation);
 
+
+char* getConsoleInputString();
+int indexOfChar(char* text, char c);
 
 static const UInt32 getConsoleStringLocation = 0x71B160;
 static const UInt32 sendCharToInput = 0x71B210;
@@ -46,6 +54,20 @@ bool NVSEPlugin_Query(const NVSEInterface * nvse, PluginInfo * info)
 			_ERROR("incorrect runtime version (got %08X need at least %08X)", nvse->runtimeVersion, RUNTIME_VERSION_1_4_0_525);
 			return false;
 		}
+
+#ifdef NOGORE
+		if(!nvse->isNogore)
+		{
+			_ERROR("incorrect runtime edition (got %08X need %08X (nogore))", nvse->isNogore, 1);
+			return false;
+		}
+#else
+		if(nvse->isNogore)
+		{
+			_ERROR("incorrect runtime edition (got %08X need %08X (standard))", nvse->isNogore, 0);
+			return false;
+		}
+#endif
 	}
 	else
 	{
@@ -54,9 +76,14 @@ bool NVSEPlugin_Query(const NVSEInterface * nvse, PluginInfo * info)
 			_ERROR("incorrect editor version (got %08X need at least %08X)", nvse->editorVersion, CS_VERSION_1_4_0_518);
 			return false;
 		}
+#ifdef NOGORE
+		_ERROR("Editor only uses standard edition, closing.");
+		return false;
+#endif
 	}
 
 	// version checks pass
+
 	return true;
 }
 
@@ -68,7 +95,6 @@ bool NVSEPlugin_Load(const NVSEInterface * nvse)
     g_DIHookCtrl = (DIHookControl*)nvseData->GetSingleton(NVSEDataInterface::kNVSEData_DIHookControl);
 
 	patchOnConsoleInput();
-
     return true;
 }
 
@@ -83,7 +109,6 @@ void patchOnConsoleInput() {
 
 void PrintClipBoardToConsoleInput() {
     GetClipboardText(&clipboardText);
-    
 	char c;
     for(int i=0,c=clipboardText[0]; c != '\0' && i < 256 /* limit size to buffer size */; i++) {
 		c = clipboardText[i];
@@ -91,6 +116,7 @@ void PrintClipBoardToConsoleInput() {
 		  /* replace newlines with spaces */
           case '\n':
 		  case '\r':
+			  
 			  PrintToConsoleInput(' ');
 			  break;
 		  /* remove pipe characters */
@@ -150,6 +176,41 @@ void __fastcall PrintToConsoleInput(UInt32 characterToPrint)
 	}
 }
 
+char* getConsoleInputString() {
+    __asm
+	{
+        mov eax, dword ptr ds:[0x11F33A8]
+        mov eax, dword ptr ds:[eax+0x14]
+	}
+}
+
+int indexOfChar(char* text, char c) {
+    int index = 0;
+	for (;text[index]!='\0'; index++)
+        if (text[index] == c)
+            return index;
+
+    return -1;
+}
+
+void DeletePreviousWord() {
+    char* consoleInput = getConsoleInputString();
+	if (!consoleInput || !strlen(consoleInput)) return;
+
+	int caretIndex = indexOfChar(consoleInput, '|');
+}
+
+void DeleteNextWord() {
+	;
+}
+
+void MoveToStartOfWord() {
+	;
+}
+
+void MoveToEndOfWord() {
+	;
+}
 
 /*
   EAX will contain the character pressed, v's code is 0x76
@@ -160,24 +221,54 @@ __declspec(naked) void __fastcall CheckCTRLV()
 	static const UInt32 retnAddr = 0x70E0A3;
 	__asm
 	{
-		push ecx
-	  checkV:
-        cmp eax, 0x76 // compare input to 'v'
-        jne handleNormalInput
-	  checkControl:	
+		push ecx // keep the address for later
+	  checkControl:
         mov		ecx, g_DIHookCtrl
 		cmp		byte ptr [ecx+0xCF], 0 // check left control
-		jne		handlePrint
+		jne		checkOtherKeys
 		cmp		byte ptr [ecx+0x44F], 0 // check right control
 		je		handleNormalInput
-      handlePrint:
+      
+    checkOtherKeys:
+      
+      checkV:
+        cmp eax, 0x76 // compare input to 'v'
+        je handleV
+	  
+      checkBackSpace:
+		cmp eax, 0x4C4B400
+		je handleBack
+      checkLeftArrow:
+		cmp eax, 0x4C4B401
+		je handleLeft
+      checkRightArrow:
+		cmp eax, 0x4C4B402
+	    je handleRight
+      checkDelete:
+		cmp eax, 0x4C4B407
+		je handleDelete
+      
+      handleV:
         call PrintClipBoardToConsoleInput
-		pop ecx
         jmp done
+      handleBack:
+        call DeletePreviousWord
+        jmp done
+      handleLeft:
+        call MoveToStartOfWord
+        jmp done
+      handleRight:
+        call MoveToEndOfWord
+        jmp done
+      handleDelete:
+        call DeleteNextWord
+        jmp done
+	  
       handleNormalInput:
-		pop ecx
         call sendCharToInput
+		jmp retnAddr
       done:
+		pop ecx
 		jmp retnAddr
   }
 }
